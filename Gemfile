94cc4ce756771092a23f5cd28c0972959b15d440
Hashma <shahi22h@mtholyoke.edu>
2015-09-12 10:35:12 -0400

    Updated footer for temp only

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ gem 'devise'
 
 gem 'rails_12factor', group: :production
 
+
+gem 'dotenv-rails', group: :development
+
 gem 'pg', group: :production
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.3'
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+MAILCHIMP_API_KEY = "e3160d9b5e689d01b6c1062b3cb8034f-us11"
+MAILCHIMP_LIST_ID = " 793aee8ba6"
